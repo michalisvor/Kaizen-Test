f57259f2058d20329cb4b00ceadbf17bb9f3209a
Michalis Vorisis <michalis.vorisis@atcom.gr>
2022-07-05 21:17:10 +0300

    Add pods and git

---
+++ Kaizen/Kaizen/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Kaizen
+//
+//  Created by Michalis Vorisis on 5/7/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
