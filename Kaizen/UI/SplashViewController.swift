ac3e7f0e1f40baf1ec5e338951d61018f5a96cce
Michalis Vorisis <michalis.vorisis@atcom.gr>
2022-07-05 22:20:18 +0300

    Add app icon, launch and splash

---
+++ Kaizen/UI/SplashViewController.swift
@@ -0,0 +1,15 @@
+//
+//  Created by Michalis Vorisis.
+//  Copyright © 2022 Michalis Vorisis. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
