bd0198ef0f8d03a2de5d4f9b97c6b12f8aa9be1c
Michalis Vorisis <michalis.vorisis@atcom.gr>
2022-07-06 22:09:33 +0300

    fixes in UI

--- Kaizen/UI/Dashboard/Helper/Cell/Collection/CollectionViewTableViewCell.swift
+++ Kaizen/UI/Dashboard/Helper/Cell/Collection/CollectionViewTableViewCell.swift
@@ -88,7 +88,7 @@ extension CollectionViewTableViewCell: UICollectionViewDelegateFlowLayout {
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        return UIEdgeInsets.zero
     }
         
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
