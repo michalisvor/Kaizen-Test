d89b06f4f4afb069b937a5ae48d51204f6730f0e
Michalis Vorisis <michalis.vorisis@atcom.gr>
2022-07-07 20:27:32 +0300

    add uikit and foundation extensions

---
+++ KaizenTests/Core/Extensions/UIKitExtensionTests.swift
@@ -0,0 +1,60 @@
+//
+//  Created by Michalis Vorisis.
+//  Copyright © 2022 Michalis Vorisis. All rights reserved.
+//
+
+import Foundation
+import XCTest
+@testable import Kaizen
+
+class UIKitExtensionTests: XCTestCase {
+     
+    func testNameOfController() {
+        let splashController = SplashViewController.id
+        
+        XCTAssertEqual(splashController, "SplashViewController")
+    }
+    
+    func testHideLoadingView() {
+        let splashController = SplashViewController.instance()
+        
+        splashController.showLoadingView()
+        XCTAssertTrue(splashController.view.subviews.contains(where: { $0 is UIActivityIndicatorView }) )
+
+        splashController.hideLoadingView()
+        XCTAssertTrue(!splashController.view.subviews.contains(where: { $0 is UIActivityIndicatorView }) )
+    }
+    
+    func testPrepareNavigationTitle() {
+        let splashController = SplashViewController.instance()
+        
+        splashController.prepareNavigationTitle("Title")
+        
+        let titleView = splashController.navigationItem.titleView as? UILabel
+        XCTAssertNotNil(titleView)
+        
+        XCTAssertEqual(titleView?.text, "Title")
+    }
+    
+    func testEmptyPrepareNavigationTitle() {
+        let splashController = SplashViewController.instance()
+        
+        splashController.prepareNavigationTitle("")
+        
+        let titleView = splashController.navigationItem.titleView as? UILabel
+        XCTAssertNotNil(titleView)
+        
+        XCTAssertEqual(titleView?.text, "")
+    }
+    
+    func testNilPrepareNavigationTitle() {
+        let splashController = SplashViewController.instance()
+        
+        splashController.prepareNavigationTitle()
+        
+        let titleView = splashController.navigationItem.titleView as? UILabel
+        XCTAssertNil(titleView)
+        
+        XCTAssertEqual(titleView?.text, nil)
+    }
+}
